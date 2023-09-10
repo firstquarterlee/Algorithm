07ec3d4671871e1cec1220e9b3c637162ee2d889
이상현 <138273378+firstquarterlee@users.noreply.github.com>
2023-09-10 22:46:51 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.32 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] array) {
+        Arrays.sort(array);
+        return array[array.length/2];
+    }
+}
+
+// Arrays.sort()을 이용하여 배열(Array)을 정렬(sorting)할 수 있습니다. ㅇㅋ 이거로 가보자.
