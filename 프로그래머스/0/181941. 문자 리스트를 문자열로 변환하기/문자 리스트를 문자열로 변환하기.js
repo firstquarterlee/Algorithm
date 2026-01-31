bda4b7ca7ded356bc45a36499d5448109d48d985
leessang <firstquarterlee@gmail.com>
2026-01-31 23:50:51 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.js
@@ -0,0 +1,11 @@
+// function solution(arr) {
+//     return arr.join('');
+// }
+
+function solution(arr) {
+    let result = "";
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i]
+    }
+    return result
+}
